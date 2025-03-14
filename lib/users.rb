132bbdf44edb73d2ca7609fb36255ad6960d5695
Parneel B <pbhakhrism@gmail.com>
2025-03-14 21:48:27 +0000

    start work on get_all_users function

--- lib/users.rb
+++ lib/users.rb
@@ -15,21 +15,28 @@ module Users
     ]
     case prompt.select('Users', options)
     when 'Get all users'
+      get_all_users_as_hash(conn)
     when 'Get user information'
     when 'Create user'
-      create_user(conn, prompt.ask('Name:'))
+      user = create_user(conn, prompt.ask('Name:'))
+      puts "Created user #{user['name']} with ID #{user['id']}"
     when 'Update user'
     when 'Delete user'
     end
   end
 
   def create_user(conn, name)
-    # Create a new user given a Postgres connection object and the user's name
-    puts conn.exec("INSERT INTO users (name) VALUES ('#{name}')")
+    # Create a new user given a Postgres connection object and the user's name, returns the id and name in a hash
+    conn.exec("INSERT INTO users (name) VALUES ('#{name}') RETURNING id, name")[0]
   end
 
   def get_all_users_as_hash(conn)
     # Get a hash of all users given a Postgres connection object
-
+    res = conn.exec('SELECT * FROM users')
+    all_fields = [res.field_values('id'), res.field_values('name'), res.field_values('creation_date'), res.field_values('books')]
+    
+    all_fields[0].length.times do |index|
+      puts "ID #{all_fields[0][index]}, name #{all_fields[1][index]}, creation_date #{all_fields[2][index].strftime("%b %e, %Y %r %Z")}, books #{all_fields[3][index]}}"
+    end
   end
 end
