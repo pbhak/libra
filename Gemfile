407dd6d26dd8ec61d92b580c5ed8b30a4d4938a0
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-03-16 01:50:47 -0700

    implement table functionality, start on delete func

--- Gemfile
+++ Gemfile
@@ -9,3 +9,5 @@ gem "pg", "~> 1.5"
 gem "dotenv", "~> 3.1"
 
 gem "tty-spinner", "~> 0.9.3"
+
+gem "tty-table", "~> 0.12.0"
--- lib/users.rb
+++ lib/users.rb
@@ -1,5 +1,6 @@
 require 'dotenv'
 require 'tty-prompt'
+require 'tty-table'
 
 # Module for user handling (CRUD operations)
 # This module assumes an already-functional PostgreSQL connection
@@ -22,6 +23,11 @@ module Users
       puts "Created user #{user['name']} with ID #{user['id']}"
     when 'Update user'
     when 'Delete user'
+      if delete_user(conn, prompt.ask('ID of user to delete:'))
+        puts 'User deleted successfully'
+      else
+        puts 'Error deleting user'
+      end
     end
   end
 
@@ -33,10 +39,18 @@ module Users
   def get_all_users_as_hash(conn)
     # Get a hash of all users given a Postgres connection object
     res = conn.exec('SELECT * FROM users')
+    table = TTY::Table.new(['ID', 'Name', 'Creation Date', '# Books'], [])
     all_fields = [res.field_values('id'), res.field_values('name'), res.field_values('creation_date'), res.field_values('books')]
     
     all_fields[0].length.times do |index|
-      puts "ID #{all_fields[0][index]}, name #{all_fields[1][index]}, creation_date #{all_fields[2][index].strftime("%b %e, %Y %r %Z")}, books #{all_fields[3][index]}}"
+      table << [all_fields[0][index], all_fields[1][index], all_fields[2][index].strftime("%m/%e/%y %r UTC%-:::z"), all_fields[3][index].length]
     end
+
+    puts table.render(:unicode)
+  end
+
+  def delete_user(conn, id)
+    # Given a connection object and a user ID, attempt to delete the user with that ID.
+    # Returns the ID of the deleted user if succesful, and false if otherwise (e.g. the user does not exist)
   end
 end
