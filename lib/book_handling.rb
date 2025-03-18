caa0f5177bc01a606c368b177b9d6d47e57bccf5
Parneel B <pbhakhrism@gmail.com>
2025-03-18 18:30:09 +0000

    fix insertion errors

--- lib/book_handling.rb
+++ lib/book_handling.rb
@@ -8,6 +8,10 @@ module BookHandling
     (Time.now + (3 * 7 * 24 * 60 * 60)).strftime('%-m/%-e/%y %r UTC%-:::z')
   end
 
+  def now
+    Time.now.strftime('%-m/%-e/%y %r UTC%-:::z')
+  end
+
   def add_book(isbn, conn)
     begin
       conn.exec("INSERT INTO books (isbn, checked_out_on, remaining_renews) VALUES (#{isbn}, NULL, NULL)")
@@ -23,11 +27,17 @@ module BookHandling
     # Check a book out to a user given the user's ID, the book ISBN, and a Postgres connection object
     # Note: queries here will be using upserts (update row instead of insert if it already exists)
     # TODO: reimplement this in coordination with add_book
+    if conn.exec("SELECT COUNT(1) FROM books WHERE isbn = #{isbn}")[0]['count'].zero?
+      puts 'Book not in library'
+      return false
+    end
+
     begin
-      conn.exec("INSERT INTO books (isbn, checked_out_to, due_on)
-                VALUES (#{isbn}, #{user_id}, #{three_weeks_from_now})
+      conn.exec("INSERT INTO books (isbn, checked_out_to, checked_out_on, due_on, remaining_renews)
+                VALUES (#{isbn}, #{user_id}, '#{now}', '#{three_weeks_from_now}, 3')
                 ON CONFLICT (isbn)
                 DO UPDATE SET isbn = EXCLUDED.isbn, checked_out_to = EXCLUDED.checked_out_to, due_on = EXCLUDED.due_on")
+      # TODO change books in users schema from integer[] to integer representing number of books instead of book ISBNs
     rescue # rubocop:disable Style/RescueStandardError
       puts 'Error inserting row'
       return false
